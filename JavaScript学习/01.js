060fd323aed88cd61fb6145f849f45a0ba28b03c
CrazyKing <twdzs013@gmail.com>
2016-08-30 18:40:14 +0800

    JavaScript0.1.1

--- JavaScript学习/01.js
+++ JavaScript学习/01.js
@@ -1,7 +1,32 @@
 /**
  * Created by Crazy_King on 16/8/29.
  */
-var message = "some string";
-alert(typeof message);
-alert(typeof (message));
-alert(typeof 95);
+
+// var name = 'abcdefg';
+//
+// if (name){
+//     alert('True');
+// }else {
+//     alert('False');
+// }
+
+// var result = Number.MAX_VALUE + Number.MAX_VALUE;
+// alert(isFinite(result));
+// 是否有限测定
+
+// var num1 = parseInt("11", 16);   //16进制到十进制
+// var num2 = parseFloat("23.34.3");
+
+
+// function doAdd() {
+//     if(arguments.length == 1){
+//         alert(arguments[0] + 10);
+//     }else if(arguments.length == 2){
+//         alert(arguments[0] + arguments[1]);
+//     }
+// }
+//
+// doAdd(10);
+// doAdd(10, 30);
+
+
